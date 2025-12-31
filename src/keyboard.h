baff43f6e6e437277d02150142255cfd90ca3750
martinsomer <martinsom35@gmail.com>
2025-12-31 18:24:41 +0200

    Remove inlining from key state update

--- src/keyboard.h
+++ src/keyboard.h
@@ -19,7 +19,7 @@ typedef struct {
 
 static KEYBOARD_STATE _kbstate;
 
-static inline void update_key_state(KEY_STATE *keystate, int key) {
+static void update_key_state(KEY_STATE *keystate, int key) {
     keystate->prev = keystate->state;
     keystate->state = (GetAsyncKeyState(key) & 0x8000) != 0;
     keystate->changed = keystate->state != keystate->prev;
